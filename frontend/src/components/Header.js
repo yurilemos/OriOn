430ae165cb41cad90821e22c459a3feb7f687966
Yuri Lemos <yuri.lemos@parafernalia.net.br>
2022-05-25 23:01:13 -0300

    controlled search component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Container, Navbar } from 'react-bootstrap';
 
+const navbarStyle = {
+    backgroundColor: 'lightblue'
+}
+
 const Header = (props) => {
     const { title } = props;
     return (
-        <Navbar bg="light" variant="light">
+        <Navbar style={navbarStyle} variant="light">
             <Container>
                 <Navbar.Brand href="/">{title}</Navbar.Brand>    
             </Container>
